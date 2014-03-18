cdf311ef6e84464a8859927ad062454cf05fac1e
Shlomi Fish <shlomif@shlomifish.org>
2014-03-18 22:44:50 +0200

    Add the hello_world.c.

---
+++ shlomif-c-snippets/hello_world.c
@@ -0,0 +1,14 @@
+/*
+ * The canonical Hello World program kept here to avoid code duplication
+ *
+ * License is the MIT/X11 license - http://opensource.org/licenses/MIT .
+ * ( https://en.wikipedia.org/wiki/MIT_License ).
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf ("%s\n", "Hello World!");
+
+    return 0;
+}
