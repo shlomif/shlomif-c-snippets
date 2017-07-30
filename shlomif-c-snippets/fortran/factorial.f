96f2cae54fbe88d08528de634cb282ebbdbcaa16
Shlomi Fish <shlomif@shlomifish.org>
2017-07-30 19:03:07 +0300

    Add the fortran program.

---
+++ shlomif-c-snippets/fortran/factorial.f
@@ -0,0 +1,8 @@
+        program factorial
+            integer :: a, b=1
+            a = 4
+                do while (a>0)
+                    b=b*a
+                    a=a-1
+                    end do
+        end
