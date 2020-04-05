01bb490c37a9f40558029916a641e01d678845f7
Shlomi Fish <shlomif@shlomifish.org>
2020-04-05 13:54:59 +0300

    add tree rec

---
+++ shlomif-c-snippets/tree_recursion.c
@@ -0,0 +1,34 @@
+/*
+ * Demonstrate tree recursion
+ *
+ * License is the MIT/Expat license - http://opensource.org/licenses/MIT .
+ * ( https://en.wikipedia.org/wiki/MIT_License ).
+ */
+#include <stdio.h>
+
+typedef int myint;
+void recurse(myint *indices, const myint max_len, const myint reached)
+{
+    if (reached == max_len)
+    {
+        for (myint i = 0; i < reached; ++i)
+        {
+            printf("%c", "abcdefghijklmnopqrstuvwxyz"[indices[i]]);
+        }
+        printf("\n");
+        return;
+    }
+    for (myint i = 0; i < 10; ++i)
+    {
+        indices[reached] = i;
+        recurse(indices, max_len, reached + 1);
+    }
+}
+
+int main(void)
+{
+    myint stack[10];
+    recurse(stack, 4, 0);
+
+    return 0;
+}
