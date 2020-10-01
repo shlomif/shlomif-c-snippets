420f724f125715ae18d96178510bec329c129d2d
Shlomi Fish <shlomif@shlomifish.org>
2020-10-01 12:59:58 +0300

    add more

---
+++ shlomif-c-snippets/newline-printf.c
@@ -0,0 +1,13 @@
+/*
+ * License is the MIT/Expat license - http://opensource.org/licenses/MIT .
+ * ( https://en.wikipedia.org/wiki/MIT_License ).
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf ("Hello World!");
+    printf ("(emcc)\n");
+
+    return 0;
+}
