7aef744319c6929aa09365798e77416132fb8056
Shlomi Fish <shlomif@shlomifish.org>
2020-10-01 15:44:38 +0300

    refactoring

--- shlomif-c-snippets/mandelbrot_set_render.cpp
+++ shlomif-c-snippets/mandelbrot_set_render.cpp
@@ -2,22 +2,23 @@
  * Copyright (c) 1996 Shlomi Fish ( https://www.shlomifish.org/ )
  * Author: Shlomi Fish ( https://www.shlomifish.org/ )
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <complex.h>
@@ -58,7 +59,9 @@ static mandelbrot_set_ret mandelbrot_set(int x1, int y1, int x2, int y2)
         return ret;
     }
     coordtype rdelta = 3.0 / (x2 - x1), idelta = 2.0 / (y2 - y1);
-    unsigned y = y1, a;
+#if WITH_GRAPHICS
+    unsigned y = y1;
+#endif
     const int init_r = 2 * (x1 - x2) / 3;
     const int final_r = (x2 - x1) / 3;
     const int final_i = (y2 - y1) / 2;
@@ -73,7 +76,7 @@ static mandelbrot_set_ret mandelbrot_set(int x1, int y1, int x2, int y2)
     {
         for (int r = init_r; r <= final_r; r++)
         {
-            a = mandelbrot_val(r * rdelta, i * idelta);
+            auto a = mandelbrot_val(r * rdelta, i * idelta);
 #if 0
             if (a)
             {
@@ -91,7 +94,9 @@ static mandelbrot_set_ret mandelbrot_set(int x1, int y1, int x2, int y2)
             printf("pos = %ld\n", ftell(f));
 #endif
         }
+#if WITH_GRAPHICS
         y++;
+#endif
     }
     fclose(f);
     return ret;
