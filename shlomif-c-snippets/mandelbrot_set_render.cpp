6a6d1a1c71a588a9b083bb003d0599890b218117
Shlomi Fish <shlomif@shlomifish.org>
2024-06-17 08:59:28 +0300

    refactor

--- shlomif-c-snippets/mandelbrot_set_render.cpp
+++ shlomif-c-snippets/mandelbrot_set_render.cpp
@@ -41,12 +41,7 @@ const coordtype MAX_NORM = 2;
 static coordtype base_norm(const ComplexType c) { return norm(c); }
 #else
 typedef long long coordtype;
-#define BREAK_ME
-#ifdef BREAK_ME
 #define PROTO_BASE 1048576
-#else
-#define PROTO_BASE 1000000
-#endif
 const coordtype BASE = PROTO_BASE;
 #if PROTO_BASE == 1048576
 static coordtype BASE_DIV(const coordtype x) { return x >> 20; }
